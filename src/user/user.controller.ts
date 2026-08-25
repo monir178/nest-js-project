8f53f036d33e92faa1a0a144fb076eae994a11f6
Md Moniruzzaman Monir <monir.mzs17@gmail.com>
2026-08-25 23:07:28 +0600

    feat: implement CRUD operations for user resource with DTO validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,41 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
 
 const USERS = [
-  { id: 1, name: 'John Doe' },
-  { id: 2, name: 'Jack Doe' },
-  { id: 3, name: 'Bob Doe' },
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jack Doe' },
+    { id: 3, name: 'Bob Doe' },
 ];
 
 @Controller('user')
 export class UserController {
-  // GET /user?name=...
-  @Get()
-  getUsers(@Query('name') name?: string) {
-    if (name) {
-      return USERS.filter((user) =>
-        user.name.toLowerCase().includes(name.toLowerCase()),
-      );
+    // GET /user?name=...
+    @Get()
+    getUsers(@Query('name') name?: string) {
+        if (name) {
+            return USERS.filter((user) =>
+                user.name.toLowerCase().includes(name.toLowerCase()),
+            );
+        }
+        return USERS;
     }
-    return USERS;
-  }
+    @Get(':id')
+    getUsersById(@Param('id') id: string) {
+        return { id, name: "John Doe" }
+    }
+    @Post()
+    createUser(@Body() CreateUserDto: CreateUserDto) {
+        return { data: CreateUserDto, message: "User created successfully" };
+    }
+    @Put(':id')
+    updateUser(@Param('id') id: string, @Body() UpdateUserDto: UpdateUserDto) {
+        return { data: { id, ...UpdateUserDto }, message: "User updated successfully" };
+    }
+    @Delete(':id')
+    deleteUser(@Param('id') id: string) {
+        return { data: id, message: "User deleted successfully" };
+    }
 }
 
